a5356b0afe91d11ba8fa2d8516708b36fed5655d
Parthiban Rajasekaran <parthibanrajasekaran@GBMB10258.local>
2023-02-17 12:50:36 +0000

    chore: adding base test for browserstack execution

--- src/test/java/com/template/project/web/tests/common/BrowserStackBaseTest.java
+++ src/test/java/com/template/project/web/tests/common/BrowserStackBaseTest.java
@@ -1,2 +1,40 @@
-package com.template.project.web.tests.common;public class BrowserStackBaseTest {
+package com.template.project.web.tests.common;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import static com.template.project.common.Logger.logInfo;
+import static com.template.project.web.utils.BrowserStackFactory.*;
+
+public class BrowserStackBaseTest {
+
+    @BeforeSuite(alwaysRun=true)
+    public void beforeSuite() {
+        getBrowserStackAccessKey();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void browserConfig(){
+        setBrowserCapabilities();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    protected static void tearDown(ITestResult testResult) {
+        tearDownBrowser();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() {
+        if (local != null) local.stop();
+    }
+
+    public static void tearDownBrowser() {
+        logInfo("Closing Browser");
+        if (driver != null) {
+            driver.close();
+        }
+    }
 }
