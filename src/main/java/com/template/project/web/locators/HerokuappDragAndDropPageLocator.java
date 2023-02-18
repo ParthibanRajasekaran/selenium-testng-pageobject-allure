5e0f1ce1b29e1b7f587d0d4fe32087f0473bae1f
Parthiban Rajasekaran <parthibanrajasekaran@GBMB10258.local>
2023-02-18 23:52:41 +0000

    chore: add drag and drop page with locator

--- src/main/java/com/template/project/web/locators/HerokuappDragAndDropPageLocator.java
+++ src/main/java/com/template/project/web/locators/HerokuappDragAndDropPageLocator.java
@@ -1,6 +1,14 @@
-package com.template.project.web.pages;
+package com.template.project.web.locators;
 
-public class HerokuappDragAndDropPage {
+public class HerokuappDragAndDropPageLocator {
+
+    protected static final String PAGE_HEADER = ".//h3";
+
+    protected static final String SOURCE_ELEMENT = "//div[@id='column-a']";
+
+    protected static final String TARGET_ELEMENT = "//div[@id='column-b']";
+
+    protected static final String TARGET_ELEMENT_META = "//div[@id='column-b']/meta";
 
 
 
--- src/main/java/com/template/project/web/pages/HerokuappDragAndDropPage.java
+++ src/main/java/com/template/project/web/pages/HerokuappDragAndDropPage.java
@@ -1,2 +1,41 @@
-package com.template.project.web.pages;public class HerokuappDragAndDropPage {
+package com.template.project.web.pages;
+
+import com.template.project.web.locators.HerokuappDragAndDropPageLocator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static com.template.project.common.Logger.logInfo;
+import static com.template.project.web.utils.SeleniumUtils.getElementText;
+import static com.template.project.web.utils.SeleniumUtils.tryFindElement;
+import static com.template.project.web.utils.Waiters.waitUntilElementVisible;
+import static com.template.project.web.utils.WebDriverHolder.getDriver;
+
+public class HerokuappDragAndDropPage extends HerokuappDragAndDropPageLocator {
+
+    public void verifyIfPageHeaderIsDisplayed() {
+        waitUntilElementVisible(PAGE_HEADER);
+        logInfo("Page header:" + getElementText(PAGE_HEADER) + " is displayed");
+    }
+
+    public void dragAndDropSourceElementToTargetElement(){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+
+        // Create an instance of Actions class
+        Actions actions = new Actions(getDriver());
+
+        WebElement sourceElement = tryFindElement(SOURCE_ELEMENT);
+        WebElement targetElement = tryFindElement(TARGET_ELEMENT);
+
+        wait.until(ExpectedConditions.elementToBeClickable(sourceElement));
+        wait.until(ExpectedConditions.elementToBeClickable(targetElement));
+
+        // Use the dragAndDrop() method to perform drag and drop
+        actions.dragAndDrop(sourceElement, targetElement).perform();
+    }
+
 }
